bf8ce4e2b45c6e7f85790c1858fdcb262c8ccc83
Kentucky94 <lis5043@gmail.com>
2020-05-31 13:20:09 +0600

    minor changes

--- frontend/src/components/MasterTable/MasterTable.js
+++ frontend/src/components/MasterTable/MasterTable.js
@@ -165,7 +165,7 @@ EnhancedTableHead.propTypes = {
 export default function EnhancedTable(props) {
   const classes = useStyles();
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('name');
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [openTable, setOpenTable] = React.useState(false);
