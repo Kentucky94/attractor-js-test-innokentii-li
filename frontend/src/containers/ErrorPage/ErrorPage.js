45a1d26d9a15c2497f8717128e9aca9f0d97d47d
Kentucky94 <lis5043@gmail.com>
2020-05-31 13:39:38 +0600

    minor interface changes

--- frontend/src/containers/ErrorPage/ErrorPage.js
+++ frontend/src/containers/ErrorPage/ErrorPage.js
@@ -5,11 +5,11 @@ class ErrorPage extends Component {
     return (
       <div className='d-flex flex-column align-items-center'>
         <h3>Hello, welcome!</h3>
-        <p>To access an admin page, please register as user with administrator rights.</p>
-        <p>For that simply use the following registration data:</p>
+        <p className='text-center'>To access an admin page, please register as user with administrator rights.</p>
+        <p className='text-center'>For that simply use the following registration data:</p>
         <p><b>Username:</b> user1</p>
         <p><b>Password:</b> password1</p>
-        <p>
+        <p className='text-center'>
           Here you can also find additional endpoints to get &nbsp;
           <a href="http://localhost:8080/categories">categories</a> and &nbsp;
           <a href="http://localhost:8080/articles">articles</a> in JSON format.
